e042e6c65c68c4b61f5ae70ed45d761eb27c3fe8
Uma-gopinathan <109015840+Uma-gopinathan@users.noreply.github.com>
2022-08-28 23:09:19 +0530

    Add files via upload

---
+++ utils/ReadData.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadData {
+	
+	public static String[][] readData(String excelName) throws IOException {		
+		
+		XSSFWorkbook wbook = new XSSFWorkbook("testData/"+excelName+".xlsx");
+		XSSFSheet sheet = wbook.getSheetAt(0); // read the first sheet
+		int rowCount = sheet.getLastRowNum();
+		short columnCount = sheet.getRow(0).getLastCellNum();
+
+		String[][] data = new String[rowCount][columnCount];
+		
+		
+		for(int i=1;i <= rowCount;i++) {
+			XSSFRow row = sheet.getRow(i);
+			for (int j = 0; j < columnCount; j++) { // looping through each cell of the first row.
+				XSSFCell cell = row.getCell(j);
+				System.out.println(cell.getStringCellValue());
+				data[i-1][j] = cell.getStringCellValue();
+				//data[i-1][j] = (String)cell.getStringCellValue();
+			}
+		}
+		
+		wbook.close();
+		return data;
+		
+	}
+
+}
